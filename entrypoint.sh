533ffffd2e6236dd988d3d8d2f878b3167199865
Tobias Draeger <tobias.draeger@comsysto.com>
2019-11-06 13:23:40 +0100

    Added username and password as input parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
-echo "Logging in to cf api $1 org $2 and space $3"
+api=$1
+org=$2
+space=$3
+user=$4
+password=$5
+
+echo "Logging in to cf api $api org $org and space $space"
 
 echo ::set-output name=deploymentResult::"successful"
