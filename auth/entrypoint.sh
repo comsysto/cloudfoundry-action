2486d2c3e072f9fde9f65002fa5c95b1e01a2ebe
Tobias Draeger <tobias.draeger@comsysto.com>
2019-11-07 09:44:42 +0100

    Edited auth/entrypoint.sh

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -1,22 +1,13 @@
-#!/bin/sh -l
+#!/bin/sh
+set -e
 
 api=$1
 user=$2
 password=$3
 
-echo "Loggin in to cloudfoundry at $api"
 cf api "$api"
-echo ::set-output name=authResult::"$(cf auth "$user" "$password")"
-# authenticationResult="$(cf auth "$user" "$password")"
-# case "$authenticationResult" in
-#  *OK*)
-#    echo ::set-output name=authResult::"$(cf auth "$user" "$password")"
-#    ;;
-#  *)
-#    echo ::set-output name=authResult::"Login failed"
-#    exit 1
-#    ;;
-# esac
+cf auth "$user" "$password"
+
 
 
 
